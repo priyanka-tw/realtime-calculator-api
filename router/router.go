b33103ecd2a1564f500588852c2417d649bffb09
Priyanka Uppalwar <priyanka11uppalwar@gmail.com>
2020-12-27 14:32:57 +0530

    add a cors middleware in order to make cross origin api calls

--- router/router.go
+++ router/router.go
@@ -7,6 +7,7 @@ import (
 
 func InitializeRouter() *gin.Engine {
 	engine := gin.Default()
+	engine.Use(corsMiddleware)
 
 	calculatorService := calculator.NewCalculatorService()
 	calculatorHandler := calculator.NewCalculatorHandler(calculatorService)
@@ -14,4 +15,10 @@ func InitializeRouter() *gin.Engine {
 	engine.POST("/calculate", calculatorHandler.Calculate)
 
 	return engine
+}
+
+func corsMiddleware(ctx *gin.Context) {
+	ctx.Writer.Header().Set("Content-Type", "application/json")
+	ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	ctx.Writer.Header().Set("Access-Control-Allow-Headers","Content-Type,access-control-allow-origin, access-control-allow-headers")
 }
