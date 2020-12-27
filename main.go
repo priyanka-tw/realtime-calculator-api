816a9388159dcba22e26377a6dab6969312d43e7
Priyanka Uppalwar <priyanka11uppalwar@gmail.com>
2020-12-27 20:50:19 +0530

    take port for runnin the app as an env variable for deployment purpose

--- main.go
+++ main.go
@@ -3,16 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"realtime-calculator-api/router"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	log.Println("initializing routes..")
 	engine := router.InitializeRouter()
 
 	log.Println("starting server on :8080")
-	err := http.ListenAndServe(":8080", engine)
-	if err != nil{
+	err := http.ListenAndServe(":"+port, engine)
+	if err != nil {
 		log.Fatal("unable to start server, err: ", err)
 	}
 
